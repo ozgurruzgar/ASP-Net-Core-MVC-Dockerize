38cbf52416e1ebafcf1a4a93d0c84e9a83e985f2
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-06-29 02:41:42 +0300

    Add project files.

---
+++ AspNetCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IFileProvider _fileProvider;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider, IConfiguration configuration)
+        {
+            _logger = logger;
+            _fileProvider = fileProvider;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.MySqlCon = _configuration["MySqlCon"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ImageShow() 
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/Images").ToList().Select(x=>x.Name);
+
+            return View(images);
+        }
+
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/Images").ToList().First(x => x.Name == name);
+
+            System.IO.File.Delete(file.PhysicalPath);
+
+            return RedirectToAction("ImageShow");
+        }
+
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile formFile)
+        {
+            if(formFile!= null && formFile.Length>0)
+            {
+                var fileName = Guid.NewGuid().ToString()+ Path.GetExtension(formFile.FileName);
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+                using (var stream = new FileStream(path,FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+////<form asp-action="ImageSavee" enctype="multipart/form-data">
+
+//    < input type = "file" name = "formfile" />
+
+//    < button type = "submit" > Resim Kaydet </ button >
+
+//</ form >
